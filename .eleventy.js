5e3be7c3b0b86fd6b4860e22e39b02f0a2fcf3ae
tyckobra <tycko.bergman@elev.ga.ntig.se>
2022-12-06 14:52:09 +0100

    22-12-06

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
@@ -9,6 +11,8 @@ config.addFilter('dateFilter', dateFilter);
 config.addFilter('w3DateFilter', w3DateFilter);
 
   config.addPassthroughCopy('./src/images/');
+  // Plugins
+config.addPlugin(rssPlugin);
  // Returns work items, sorted by display order
 config.addCollection('work', collection => {
   return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
@@ -25,6 +29,15 @@ config.addCollection('featuredWork', collection => {
     x => x.data.featured
   );
 });
+
+
+// Returns a list of people ordered by filename
+config.addCollection('people', collection => {
+  return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+    return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+  });
+});
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
--- src/blog.md
+++ src/blog.md
@@ -1,5 +1,5 @@
 ---
-title: 'The Issue 33 Blog'
+title: 'The Issue 69 Blog'
 layout: 'layouts/feed.html'
 pagination:
   data: collections.blog
